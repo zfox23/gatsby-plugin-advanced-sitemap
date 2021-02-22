95c8c568d3f04c88361b523f70928cdb52bda4dc
Abraham Elmahrek <abe@apache.org>
2021-02-22 13:43:22 -0800

    Add outputFile

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,7 @@ import pify from 'pify';
 export const withoutTrailingSlash = path => (path === `/` ? path : path.replace(/\/$/, ``));
 
 export const writeFile = pify(fs.writeFile);
+export const outputFile = pify(fs.outputFile);
 export const renameFile = pify(fs.rename);
 export const readFile = pify(fs.readFile);
 
